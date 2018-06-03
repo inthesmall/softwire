91a72cd8f9b7bc366d1e92b00d0478a7b3091f63
Ethan Mills <ethan.mills@hotmail.co.uk>
2018-06-03 12:01:16 +0100

    Added multiplier

--- js/calc/index.js
+++ js/calc/index.js
@@ -1,1 +1,9 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+console.log('Enter first number to multiply:');
+const first = readline.prompt();
+console.log('Enter second number to multiply:');
+const second = readline.prompt();
+const res = first * second;
+console.log("The result is " + res);
