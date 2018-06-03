97d215753d6c6a0210162f1500f5f978fc4ade36
Ethan Mills <ethan.mills@hotmail.co.uk>
2018-06-03 14:01:21 +0100

    Added plus, minus, times and divide functionality.

--- js/calc/index.js
+++ js/calc/index.js
@@ -1,9 +1,35 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
-console.log('Enter first number to multiply:');
-const first = readline.prompt();
-console.log('Enter second number to multiply:');
-const second = readline.prompt();
-const res = first * second;
+console.log('Please enter an operator (+, -, *, or /):');
+const op = readline.prompt();
+console.log('Enter first number:');
+const first = parseFloat(readline.prompt());
+console.log('Enter second number:');
+const second = parseFloat(readline.prompt());
+
+// Perform the requested operation
+
+function operate(op, first, second) {
+	switch (op){
+		case "+":
+			var res = first + second;
+			break;
+		case "-":
+			res = first - second;
+			break;
+		case "*":
+			res = first * second;
+			break;
+		case "/":
+			res = first / second;
+			break;
+		default:
+			console.log("The operator you provided was not in the list");
+			break;
+	}
+	return res;
+}
+
+var res = operate(op, first, second);
 console.log("The result is " + res);
